bc3515cf718428fa676ee0e42899e56030ce3c43
Federico-Audizio <federicoaudizio08@gmail.com>
2024-11-01 12:22:29 -0300

    Mas facha
    Están fachas las pantallas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth-ktx")
 
+
     //
 
     implementation ("com.google.android.gms:play-services-maps:18.0.0")
